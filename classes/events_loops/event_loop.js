dfa9364bc39998e5bb2696a428f319cab0151297
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-01-24 20:06:13 -0300

    Add event loop demonstration with file reading and password encryption

--- classes/events_loops/event_loop.js
+++ classes/events_loops/event_loop.js
@@ -1,4 +1,8 @@
 import fs from 'fs';
+import crypto from 'crypto';
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 1;
 
 setTimeout(() => {
     console.log('This message is logged after 2 seconds');
@@ -8,10 +12,20 @@ setImmediate(() => {
     console.log('This message is logged immediately after I/O events');
 });
 
+// In the parameters fisrt of all, error after the data that you want to display.
 fs.readFile('C:/Users/ariel/OneDrive/Documentos/GitHub/nodejs-learning/classes/events_loops/test_file.txt', 'utf-8', (err, data) => {
     if(err){
         console.error("Erro: " + err);
         return;
     }
     console.log(data);
+
+    process.nextTick(() => {
+        console.log("Process.nextTick");
+    });
+
+    crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start, "Password encrypted.");
+        // Date.now() - start, "Password encrypted.") basically it means how much seconds it took to encrypted the message.
+    });
 });
