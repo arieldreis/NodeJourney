fde1da52f957d8c9fad17b94346df6938c4034da
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-04-23 11:00:45 -0300

    Add React 19.2.5 with server support and runtime files
    - Introduced new production and development builds for React, including:
      - react.react-server.production.js
      - react.react-server.development.js
      - compiler-runtime.js
      - jsx-runtime.js
      - jsx-dev-runtime.js
      - jsx-runtime.react-server.js
      - jsx-dev-runtime.react-server.js
    - Updated package.json to include new files and exports for server and runtime modules.
    - Ensured compatibility with Node.js environments by conditionally exporting modules based on NODE_ENV.

--- classes/express/server.js
+++ classes/express/server.js
@@ -8,8 +8,12 @@ const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Caminho do arquivo
+const tourSimpleFile = path.join(__dirname, '.', 'toursSimple.json');
+// Transformando o arquivo eem formato JSON.
+const tours = JSON.parse(fs.readFileSync(`${tourSimpleFile}`));
+
 app.get('/api/v1/tours', (req, res) => {
-    const tourSimpleFile = path.join(__dirname, '.', 'toursSimple.json');
     try{
         const readFile = fs.readFileSync(tourSimpleFile);
         return res.status(200).json({
@@ -23,8 +27,45 @@ app.get('/api/v1/tours', (req, res) => {
     }
 });
 
-app.post('/api/v1/tours', (req, res) => {
-    res.send("You can post to this endpooint");
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    try{
+        // Status 404
+        if(!tour){
+            return res.status(404).json({
+                status: '404',
+                message: 'Invalid ID.'
+            });
+        }
+        // Status 200
+        const readFile = fs.readFileSync(tourSimpleFile);
+        if(tour){
+            return res.status(200).json({
+                status: 'success',
+                data: {
+                    tours: tour
+                }
+            });
+        }
+    } catch (err) {
+        console.error('Erro:', erro.message);
+    }
+});
+
+app.post('/api/v1/create', (req, res) => {
+   const newId = tours.length > 0 ? tours[tours.length - 1].id + 1 : 1;
+   const newTour = Object.assign({ id: newId }, req.body);
+
+   tours.push(newTour);
+   fs.writeFile(tourSimpleFile, JSON.stringify(tours), err => {
+    res.status(201).json({
+        status: "success",
+        data: {
+            tour: newTour
+        }
+    });
+   });
 });
 
 app.listen(PORT, () => {
